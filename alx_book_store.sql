58097b5fad12ccffab25ca772308090fd26e79f9
itsaiight <mahlaregosiame@gmail.com>
2025-06-25 20:50:28 +0200

    Created Intro_to_DB

---
+++ alx_book_store.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE Books (
+book_id (PRIMARY KEY),
+title VARCHAR(130),
+author_id (Foreign Key), --referencing Authors table
+price DOUBLE,
+publication_date DATE
+);
+
+CREATE TABLE Authors (
+author_id (PRIMARY KEY),
+author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+customer_id (PRIMARY KEY),
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE Orders (
+order_id (PRIMARY KEY),
+customer_id (Foreign Key), --referencing Customers table
+order_date DATE
+);
+
+CREATE TABLE Order_Details (
+orderdetailid (PRIMARY KEY),
+order_id (Foreign Key), --referencing Orders table
+book_id (Foreign Key), -- referencing Books table
+quantity DOUBLE
+);
