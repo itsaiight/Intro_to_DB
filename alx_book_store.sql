6a6b0edcb4ae7b3fc865b483be7f76229fce054d
itsaiight <mahlaregosiame@gmail.com>
2025-06-25 21:00:20 +0200

    Updated alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,34 +1,34 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Books (
-book_id (PRIMARY KEY),
+book_id INT (PRIMARY KEY),
 title VARCHAR(130),
-author_id (Foreign Key), --referencing Authors table
+author_id INT (Foreign Key), --referencing Authors table
 price DOUBLE,
 publication_date DATE
 );
 
 CREATE TABLE Authors (
-author_id (PRIMARY KEY),
+author_id INT (PRIMARY KEY),
 author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers (
-customer_id (PRIMARY KEY),
+customer_id INT (PRIMARY KEY),
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 
 CREATE TABLE Orders (
-order_id (PRIMARY KEY),
-customer_id (Foreign Key), --referencing Customers table
+order_id INT (PRIMARY KEY),
+customer_id INT (Foreign Key), --referencing Customers table
 order_date DATE
 );
 
 CREATE TABLE Order_Details (
-orderdetailid (PRIMARY KEY),
-order_id (Foreign Key), --referencing Orders table
-book_id (Foreign Key), -- referencing Books table
+orderdetailid INT (PRIMARY KEY),
+order_id INT (Foreign Key), --referencing Orders table
+book_id INT (Foreign Key), -- referencing Books table
 quantity DOUBLE
 );
