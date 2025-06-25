16c8c413abd598485980616bb7d5bdd982abba54
itsaiight <mahlaregosiame@gmail.com>
2025-06-25 21:08:35 +0200

    Updated alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books (
 book_id INT (PRIMARY KEY),
 title VARCHAR(130),
-FOREIGN KEY (author_id), --referencing Authors table
+FOREIGN KEY (author_id), --FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 price DOUBLE,
 publication_date DATE
 );
@@ -22,13 +22,13 @@ address TEXT
 
 CREATE TABLE Orders (
 order_id INT (PRIMARY KEY),
-FOREIGN KEY (customer_id), --"FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)"
+FOREIGN KEY (customer_id), --FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 order_date DATE
 );
 
 CREATE TABLE Order_Details (
 orderdetailid INT (PRIMARY KEY),
-FOREIGN KEY (order_id), --referencing Orders table
-FOREIGN KEY (book_id), -- referencing Books table
+FOREIGN KEY (order_id), --FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+FOREIGN KEY (book_id), --FOREIGN KEY (book_id) REFERENCES Books(book_id)
 quantity DOUBLE
 );
