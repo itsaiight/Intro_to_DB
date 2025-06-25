a79c247542f806b1e1e26187634e69b49f309af1
itsaiight <mahlaregosiame@gmail.com>
2025-06-25 21:05:54 +0200

    Updated alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,7 +22,7 @@ address TEXT
 
 CREATE TABLE Orders (
 order_id INT (PRIMARY KEY),
-FOREIGN KEY (customer_id), --referencing Customers table
+FOREIGN KEY (customer_id), --"FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)"
 order_date DATE
 );
 
